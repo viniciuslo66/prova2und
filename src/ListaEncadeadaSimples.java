14e5de0f174f6fe7481295575b347b3e1509a2b6
viniciuslo66 <viniciuslo66@gmail.com>
2021-05-03 15:10:30 -0300

    Questão 3º

---
+++ src/ListaEncadeadaSimples.java
@@ -0,0 +1,88 @@
+public class ListaEncadeadaSimples<T> {
+    class Node {
+        T data;
+        Node next;
+
+        public Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public Node head;
+    public Node tail;
+    public int nextId;
+    public int size;
+
+    public ListaEncadeadaSimples() {
+        head = null;
+        tail = null;
+        nextId = 1;
+        size = 0;
+    }
+
+    public void show() {
+        Node p = head;
+
+        if (p == null) {
+            System.out.println("LISTA VAZIA \n");
+        } else {
+            while (p != null) {
+                System.out.println("Dado: " + p.data);
+                p = p.next;
+            }
+        }
+
+        System.out.println("size = " + size + "\n");
+    }
+
+    public void addFirst(T dado) {
+        Node novo = new Node(dado);
+        if (head == null) {
+            head = novo;
+            tail = novo;
+        } else {
+            novo.next = head;
+            head = novo;
+        }
+
+        size++;
+    }
+
+    public void addLast(T dado) {
+        Node novo = new Node(dado);
+
+        if (head == null) {
+            head = novo;
+            tail = novo;
+        } else {
+            tail.next = novo;
+            tail = novo;
+        }
+
+        size++;
+    }
+
+    private Node searchNode(T criterio) {
+        Node p = head;
+
+        while (p != null) {
+            if (p.data.equals(criterio)) {
+                return p;
+            }
+            p = p.next;
+        }
+
+        return null;
+    }
+
+    public T search(T criterio) {
+        Node p = searchNode(criterio);
+
+        if (p == null) {
+            return null;
+        } else {
+            return p.data;
+        }
+    }
+}
---
+++ src/q3.java
@@ -0,0 +1,23 @@
+public class q3 {
+    public static void main(String[] args) {
+        ListaEncadeadaSimples<Integer> lista1 = new ListaEncadeadaSimples<Integer>();
+        ListaEncadeadaSimples<Integer> listaInvertida = new ListaEncadeadaSimples<Integer>();
+
+        int ultimoElemento = 9;
+
+        lista1.addLast(4);
+        lista1.addLast(5);
+        lista1.addLast(1);
+        lista1.addLast(ultimoElemento);
+
+        lista1.show();
+
+        for (int i = 0; i <= ultimoElemento; i++) {
+            if (lista1.search(i) != null) {
+                listaInvertida.addFirst(lista1.search(i));
+            } 
+        }
+
+        listaInvertida.show();
+    }
+}
